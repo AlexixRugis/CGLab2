5fc3ca46df04c35c22495c81872c1c73980bf040
Alexey Gutyansky <75169888+AlexixRugis@users.noreply.github.com>
2025-03-24 14:28:26 +0300

    started

---
+++ CGLab2/Program.cs
@@ -0,0 +1,11 @@
+﻿
+public static class App
+{
+    public static void Main()
+    {
+        using (Window window = new Window(800, 600, "LearnOpenTK"))
+        {
+            window.Run();
+        }
+    }
+}
---
+++ CGLab2/Window.cs
@@ -0,0 +1,100 @@
+﻿
+
+using OpenTK.Graphics.OpenGL4;
+using OpenTK.Windowing.Common;
+using OpenTK.Windowing.Desktop;
+using OpenTK.Windowing.GraphicsLibraryFramework;
+
+public class Window : GameWindow
+{
+    private readonly float[] _vertices =
+    {
+        0.5f, 0.5f, 0.0f,
+        0.5f, -0.5f, 0.0f,
+        -0.5f, -0.5f, 0.0f,
+        -0.5f, 0.5f, 0.0f
+    };
+
+    private readonly uint[] _indices =
+    {
+        0, 1, 3,
+        1, 2, 3
+    };
+
+    private int _vbo;
+    private int _ebo;
+    private int _vao;
+
+    private Shader _shader;
+
+    public Window(int width, int height, string title) 
+        : base(GameWindowSettings.Default, 
+            new NativeWindowSettings() { ClientSize = (width, height), Title = title })
+    {
+    }
+
+    protected override void OnLoad()
+    {
+        base.OnLoad();
+
+        GL.ClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+
+        string vpath = Path.Combine(Directory.GetCurrentDirectory(), "Resources/Shaders/vert.glsl");
+        string fpath = Path.Combine(Directory.GetCurrentDirectory(), "Resources/Shaders/frag.glsl");
+        _shader = new Shader(vpath, fpath);
+
+        _vao = GL.GenVertexArray();
+        _ebo = GL.GenBuffer();
+        _vbo = GL.GenBuffer();
+
+        GL.BindVertexArray(_vao);
+        GL.BindBuffer(BufferTarget.ArrayBuffer, _vbo);
+        GL.BufferData(BufferTarget.ArrayBuffer, _vertices.Length * sizeof(float), _vertices, BufferUsageHint.StaticDraw);
+        GL.BindBuffer(BufferTarget.ElementArrayBuffer, _ebo);
+        GL.BufferData(BufferTarget.ElementArrayBuffer, _indices.Length * sizeof(float), _indices, BufferUsageHint.StaticDraw);
+
+        GL.VertexAttribPointer(0, 3, VertexAttribPointerType.Float, false, 3 * sizeof(float), 0);
+        GL.EnableVertexAttribArray(0);
+    }
+
+    protected override void OnUnload()
+    {
+        base.OnUnload();
+
+        GL.BindBuffer(BufferTarget.ArrayBuffer, 0);
+        GL.DeleteBuffer(_vbo);
+        GL.BindBuffer(BufferTarget.ElementArrayBuffer, 0);
+        GL.DeleteBuffer(_ebo);
+        GL.BindVertexArray(0);
+        GL.DeleteVertexArray(_vao);
+
+        _shader.Dispose();
+    }
+
+    protected override void OnFramebufferResize(FramebufferResizeEventArgs e)
+    {
+        base.OnFramebufferResize(e);
+
+        GL.Viewport(0, 0, e.Width, e.Height);
+    }
+
+    protected override void OnUpdateFrame(FrameEventArgs args)
+    {
+        base.OnUpdateFrame(args);
+
+        if (KeyboardState.IsKeyDown(Keys.Escape))
+        {
+            Close();
+        }
+
+        // Rendering
+
+        GL.Clear(ClearBufferMask.ColorBufferBit);
+
+        _shader.Use();
+        GL.BindVertexArray(_vao);
+        GL.DrawElements(PrimitiveType.Triangles, _indices.Length, DrawElementsType.UnsignedInt, 0);
+
+        SwapBuffers();
+    }
+}
